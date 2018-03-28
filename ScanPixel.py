5a50cb444f890165cf83b27a0da53299d2ab9777
kangmyeongseok <kms00129@naver.com>
2018-03-28 22:41:35 +0900

    find double vertex

--- ScanPixel.py
+++ ScanPixel.py
@@ -15,7 +15,7 @@ for i in range(0,height-1):
         if(px[j,i] == (255,255,255)):
             coordinate[i][j] = 1
 
-
+"""
 for i in range(0,height-1):
     for j in range(0,width-1):
         if (j in wall):
@@ -26,7 +26,19 @@ for i in range(0,height-1):
                 wall.append(j)
                 j=j+1
             vertex.append((j-1, i))
+wall.clear()
+"""
 
+for i in range(height-1,0,-1):
+    for j in range(width-1,0,-1):
+        if (j in wall):
+            continue
+        if(coordinate[i][j] == 1):
+            vertex.append((j, i))
+            while(coordinate[i][j] == 1 and j not in wall):
+                wall.append(j)
+                j=j-1
+            vertex.append((j-1, i))
 
 
 print(vertex)
