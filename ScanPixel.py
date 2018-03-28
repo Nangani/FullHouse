7e9d94e178035af960e55290743e34f0b3c37d9c
kangmyeongseok <kms00129@naver.com>
2018-03-28 22:20:06 +0900

    find vertex

---
+++ ScanPixel.py
@@ -0,0 +1,35 @@
+from PIL import Image,ImageFilter
+
+image = Image.open("sample.png")
+px = image.load()
+vertex = []
+wallStart = []
+wallEnd = []
+wall = []
+untilStop =0
+(width,height) = image.size
+coordinate = [[0 for col in range(width)] for row in range(height)]
+print(image.size)
+for i in range(0,height-1):
+    for j in range(0,width-1):
+        if(px[j,i] == (255,255,255)):
+            coordinate[i][j] = 1
+
+
+for i in range(0,height-1):
+    for j in range(0,width-1):
+        if (j in wall):
+            continue
+        if(coordinate[i][j] == 1):
+            vertex.append((j, i))
+            while(coordinate[i][j] == 1 and j not in wall):
+                wall.append(j)
+                j=j+1
+            vertex.append((j-1, i))
+
+
+
+print(vertex)
+#print(wallEnd)
+#for row in coordinate:
+#        print (row)
