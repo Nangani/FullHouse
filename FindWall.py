0f790c7a1ba4e6662e353ac0c10713aa61641920
incitante <yangjiwan@gmail.com>
2018-04-12 21:10:42 +0900

    demo

--- FindWall.py
+++ FindWall.py
@@ -10,15 +10,8 @@ from PIL import Image
 from PIL import ImageDraw
 import requests
 
-def isWaterMark(hsv, j1, i1, j2, i2):
-    if -3 <= int(hsv[j1, i1][0]) - int(hsv[j2, i2][0]) and int(hsv[j1, i1][0]) - int(hsv[j2, i2][0]) <= 3 and -60 <= int(hsv[j1, i1][1]) - int(hsv[j2, i2][1]) and int(hsv[j1, i1][1]) - int(hsv[j2, i2][1]) <= 0\
-            and 0 <= int(hsv[j1, i1][2]) - int(hsv[j2, i2][2]) and int(hsv[j1, i1][2]) - int(hsv[j2, i2][2]) <= 30 and int(hsv[j2, i2][2]) <= 25:
-        return True
-    else:
-        return False
-
 def main(argv):
-    default_file =  "plan3.jpg"
+    default_file =  "plan.png"
     filename = argv[0] if len(argv) > 0 else default_file
 
     # Loads an image
@@ -31,79 +24,30 @@ def main(argv):
         return -1
 
     hsvImage = cv.cvtColor(src, cv.COLOR_BGR2HSV)
-    cv.imshow("hsvImage", hsvImage)
+#    cv.imshow("hsvImage", hsvImage)
     lower = np.array([[[0, 0, 0]]])
-    upper = np.array([[[255, 255, 45]]])
+    upper = np.array([[[180, 255, 90]]])
     mask = cv.inRange(hsvImage, lower, upper)
 
-    cv.imshow("mask", mask)
+ #   cv.imshow("mask", mask)
+    cv.imwrite('temp.png', mask)
+    height = np.size(src, 0)
+    width = np.size(src, 1)
 
-    aaaa = Image.open(default_file)
-    (width, height) = aaaa.size
-    print(width)
-    print(height)
-    print(hsvImage[280, 200])
-    #(width, height) = hsvi.size
-    for i in range(1, width-1):
-        for j in range(1, height-1):
-            if j - 1 >= 0 and isWaterMark(hsvImage, j, i, j - 1, i):
-               # print(hsvi[j, i])
-                #print(hsvi[j-1, i])
-               # print((int(hsvi[j, i][1]) + int(hsvi[j-1, i][1])) / 2)
-               # print(hsvi[j, i][1])
-               #
-               hsvImage[j, i][0] = hsvImage[j - 1, i][0]
-               hsvImage[j, i][1] = hsvImage[j - 1, i][1]
-               hsvImage[j, i][2] = hsvImage[j - 1, i][2]
+    #print(width)
+    #print(height)
 
-               #hsvi[j, i][1] = (int(hsvi[j, i][1]) + int(hsvi[j-1, i][1])) / 2
-               #hsvi[j, i][2] = (int(hsvi[j, i][2]) + int(hsvi[j-1, i][2])) / 2
+    #print(src[height-1][width-1])
 
-            #elif j + 1 < height and isWaterMark(hsvi, j, i, j + 1, i):
-             #   hsvi[j, i][1] = hsvi[j+1, i][1]
-              #  hsvi[j, i][2] = hsvi[j+1, i][2]
-
-            elif i - 1 >= 0 and isWaterMark(hsvImage, j, i, j, i - 1):
-                hsvImage[j, i][0] = hsvImage[j, i - 1][0]
-                hsvImage[j, i][1] = hsvImage[j, i - 1][1]
-                hsvImage[j, i][2] = hsvImage[j, i - 1][2]
-                #hsvi[j, i][1] = (int(hsvi[j, i][1]) + int(hsvi[j, i - 1][1])) / 2
-                #hsvi[j, i][2] = (int(hsvi[j, i][2]) + int(hsvi[j, i - 1][2])) / 2
-            '''
-            elif i + 1 < width and isWaterMark(hsvi, j, i, j, i + 1):
-                hsvi[j, i][1] = hsvi[j, i+1][1]
-                hsvi[j, i][2] = hsvi[j, i+1][2]
-        '''
-
-    cv.imshow("water", hsvImage[:, :, 2])
+  #  cv.imshow("water", hsvImage[:, :, 2])
 
     mask5 = hsvImage[:, :, 2]
     mask55 = cv.inRange(mask5, lower, upper)
-    cv.imshow("mask55", mask55)
+   # cv.imshow("mask55", mask55)
 
     # Copy edges to the images that will display the results in BGR
     cdst = cv.cvtColor(mask55, cv.COLOR_GRAY2BGR)
     cdstP = np.copy(cdst)
-
-    ## [hough_lines]
-    #  Standard Hough Line Transform
-    lines = cv.HoughLines(mask55, 1, np.pi / 180, 150, None, 0, 0)
-    ## [hough_lines]
-    ## [draw_lines]
-    # Draw the lines
-    if lines is not None:
-        for i in range(0, len(lines)):
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-
-            cv.line(cdst, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
-    ## [draw_lines]
 
     ## [hough_lines_p]
     # Probabilistic Line Transform
@@ -116,11 +60,11 @@ def main(argv):
             l = linesP[i][0]
             cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv.LINE_AA)
     ## [draw_lines_p]
-    ## [imshow]
+#    ## [imshow]
     # Show results
-    cv.imshow("Source", src)
-    cv.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
-    cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+ #   cv.imshow("Source", src)
+  #  cv.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
+   # cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
     ## [imshow]
     ## [exit]
     # Wait and Exit
--- PrintWall.py
+++ PrintWall.py
@@ -14,5 +14,61 @@ for i in range(0,height-1):
     for j in range(0,width-1):
         if(coordinate[i][j]*4+coordinate[i][j+1]*2+coordinate[i+1][j]*2+coordinate[i-1][j]*2+coordinate[i][j-1]*2+coordinate[i+1][j+1]*1+coordinate[i-1][j+1]*1+coordinate[i+1][j-1]*1+coordinate[i-1][j-1]*1<9):
             coordinate[i][j]=0
+"""
+111  111  001  100
+110  011  011  110
+100  001  111  111
+"""
+for i in range(0, height - 1):
+    for j in range(0, width - 1):
+        if (coordinate[i - 1][j - 1] == 1 and coordinate[i - 1][j] == 1 and coordinate[i - 1][j + 1] == 1 and coordinate[i][j - 1] == 1 and coordinate[i][j] == 1 and coordinate[i + 1][j - 1] == 1 and coordinate[i][j + 1] == 0 and coordinate[i + 1][j] == 0 and coordinate[i + 1][j + 1] == 0)\
+                or (coordinate[i - 1][j - 1] == 1 and coordinate[i - 1][j] == 1 and coordinate[i - 1][j + 1] == 1 and coordinate[i][j] == 1 and coordinate[i][j + 1] == 1 and coordinate[i + 1][j + 1] == 1 and coordinate[i][j - 1] == 0 and coordinate[i + 1][j - 1] == 0 and coordinate[i + 1][j] == 0)\
+                or (coordinate[i - 1][j + 1] == 1 and coordinate[i][j] == 1 and coordinate[i][j + 1] == 1 and coordinate[i + 1][j - 1] == 1 and coordinate[i + 1][j] == 1 and coordinate[i + 1][j + 1] == 1 and coordinate[i - 1][j - 1] == 0 and coordinate[i - 1][j] == 0 and coordinate[i][j - 1] == 0)\
+                or (coordinate[i - 1][j - 1] == 1 and coordinate[i][j - 1] == 1 and coordinate[i][j] == 1 and coordinate[i + 1][j - 1] == 1 and coordinate[i + 1][j] == 1 and coordinate[i + 1][j + 1] == 1 and coordinate[i - 1][j] == 0 and coordinate[i - 1][j + 1] == 0 and coordinate[i][j + 1] == 0):
+            coordinate[i][j] = 0
+"""
+000  110  111  011
+10   100  10   001
+111  1 0  000  0 1
+"""
+for i in range(0, height - 1):
+    for j in range(0, width - 1):
+        if (coordinate[i][j] == 0 and coordinate[i - 1][j - 1] == 0 and coordinate[i - 1][j] == 0 and coordinate[i - 1][j + 1] == 0 and coordinate[i + 1][j - 1] == 1 and coordinate[i + 1][j] == 1 and coordinate[i + 1][j + 1] == 1 and coordinate[i][j - 1] == 1)\
+                or (coordinate[i][j] == 0 and coordinate[i - 1][j + 1] == 0 and coordinate[i][j + 1] == 0 and coordinate[i + 1][j + 1] == 0 and coordinate[i - 1][j - 1] == 1 and coordinate[i][j - 1] == 1 and coordinate[i + 1][j - 1] == 1 and coordinate[i - 1][j] == 1)\
+                or (coordinate[i - 1][j - 1] == 1 and coordinate[i - 1][j] == 1 and coordinate[i - 1][j + 1] == 1 and coordinate[i][j - 1] == 1 and coordinate[i][j] == 0 and coordinate[i + 1][j - 1] == 0 and coordinate[i + 1][j] == 0 and coordinate[i + 1][j + 1] == 0)\
+                or (coordinate[i - 1][j - 1] == 0 and coordinate[i - 1][j] == 1 and coordinate[i - 1][j + 1] == 1 and coordinate[i][j - 1] == 0 and coordinate[i][j] == 0 and coordinate[i][j + 1] == 1 and coordinate[i + 1][j - 1] == 0 and coordinate[i + 1][j + 1] == 1):
+            coordinate[i][j] = 1
+"""
+0 1  1 0  111  000
+001  100   01   01
+011  110  000  111
+"""
+
+for i in range(height - 2, 0, -1):
+    for j in range(width - 2, 0, -1):
+        if (coordinate[i - 1][j - 1] == 0 and coordinate[i - 1][j + 1] == 1 and coordinate[i][j - 1] == 0 and coordinate[i][j] == 0 and coordinate[i][j + 1] == 1 and coordinate[i + 1][j - 1] == 0 and coordinate[i + 1][j] == 1 and coordinate[i + 1][j + 1] == 1)\
+                or (coordinate[i - 1][j - 1] == 1 and coordinate[i - 1][j + 1] == 0 and coordinate[i][j - 1] == 1 and coordinate[i][j] == 0 and coordinate[i][j + 1] == 0 and coordinate[i + 1][j - 1] == 1 and coordinate[i + 1][j] == 1 and coordinate[i + 1][j + 1] == 0)\
+                or (coordinate[i - 1][j - 1] == 1 and coordinate[i - 1][j] == 1 and coordinate[i - 1][j + 1] == 1 and coordinate[i][j] == 0 and coordinate[i][j + 1] == 1 and coordinate[i + 1][j - 1] == 0 and coordinate[i + 1][j] == 0 and coordinate[i + 1][j + 1] == 0)\
+                or (coordinate[i - 1][j - 1] == 0 and coordinate[i - 1][j] == 0 and coordinate[i - 1][j + 1] == 0 and coordinate[i][j] == 0 and coordinate[i][j + 1] == 1 and coordinate[i + 1][j - 1] == 1 and coordinate[i + 1][j] == 1 and coordinate[i + 1][j + 1] == 1):
+            coordinate[i][j] = 1
+"""
+000  110  011  000
+100  100  001  001
+110  000  000  011
+"""
+for i in range(0, height - 1):
+    for j in range(0, width - 1):
+        if (coordinate[i - 1][j - 1] == 0 and coordinate[i - 1][j] == 0 and coordinate[i - 1][j + 1] == 0 and coordinate[i][j - 1] == 1 and coordinate[i][j] == 0 and coordinate[i][j + 1] == 0 and coordinate[i + 1][j - 1] == 1 and coordinate[i + 1][j] == 1 and coordinate[i + 1][j + 1] == 0)\
+                or (coordinate[i - 1][j - 1] == 1 and coordinate[i - 1][j] == 1 and coordinate[i - 1][j + 1] == 0 and coordinate[i][j - 1] == 1 and coordinate[i][j] == 0 and coordinate[i][j + 1] == 0 and coordinate[i + 1][j - 1] == 0 and coordinate[i + 1][j] == 0 and coordinate[i + 1][j + 1] == 0)\
+                or (coordinate[i - 1][j - 1] == 0 and coordinate[i - 1][j] == 1 and coordinate[i - 1][j + 1] == 1 and coordinate[i][j - 1] == 0 and coordinate[i][j] == 0 and coordinate[i][j + 1] == 1 and coordinate[i + 1][j - 1] == 0 and coordinate[i + 1][j] == 0 and coordinate[i + 1][j + 1] == 0)\
+                or (coordinate[i - 1][j - 1] == 0 and coordinate[i - 1][j] == 0 and coordinate[i - 1][j + 1] == 0 and coordinate[i][j - 1] == 0 and coordinate[i][j] == 0 and coordinate[i][j + 1] == 1 and coordinate[i + 1][j - 1] == 0 and coordinate[i + 1][j] == 1 and coordinate[i + 1][j + 1] == 1):
+            coordinate[i][j] = 1
+
+for i in range(0,height-1):
+    for j in range(0,width-1):
+        if(coordinate[i][j]*4+coordinate[i][j+1]*2+coordinate[i+1][j]*2+coordinate[i-1][j]*2+coordinate[i][j-1]*2+coordinate[i+1][j+1]*1+coordinate[i-1][j+1]*1+coordinate[i+1][j-1]*1+coordinate[i-1][j-1]*1<9):
+            coordinate[i][j]=0
+        else:
+            coordinate[i][j] = 1
 
 print(coordinate)
